235435c1217ebe0ef042d67029826211c8d66f0d
Yaroslav Serhieiev <yaroslavs@wix.com>
2019-10-28 09:42:44 +0200

    APK Golf

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -20,4 +20,4 @@
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
 
--keep class com.fractalwrench.** { *; }
+-keep class b.c.** { *; }
--- app/src/main/java/b/c/MainActivity.java
+++ app/src/main/java/b/c/MainActivity.java
@@ -2,12 +2,15 @@ package b.c;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView textView = new TextView(this);
+        textView.setText("C");
+        setContentView(textView);
     }
 }
